cbd342e194b117daa91b44ab5ea1ee183b937f4d
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-10-20 18:46:26 +0600

    Create 1017. A. Trippi Troppi.cpp

---
+++ Problem - A/1017. A. Trippi Troppi.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    string a,b,c;
+ 
+    for(int i=0;i<t;i++){
+        cin>>a>>b>>c;
+        cout<<a[0]<<b[0]<<c[0]<<endl;
+    }
+ 
+    return 0;
+}
