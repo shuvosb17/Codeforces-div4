4473232451f54ba8bbfcece25a0a743bc619d904
Shuvo <129549247+shuvosb17@users.noreply.github.com>
2025-10-21 00:22:28 +0600

    Create 971. A. Minimize!.cpp

---
+++ Problem - A/971. A. Minimize!.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t,a,b;
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        cin>>a>>b;
+        int sum=b-a;
+        cout<<sum<<endl;
+    }
+
+    return 0;
+}
